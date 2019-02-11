4324b9ae852ff9bf94bd4e0b750e280d969ab726
cybervaca <cybervaca@gmail.com>
2019-02-11 17:37:58 +0100

    Update AssemblyInfo.cs

--- EvilSalsa/EvilSalsa/Properties/AssemblyInfo.cs
+++ EvilSalsa/EvilSalsa/Properties/AssemblyInfo.cs
@@ -9,12 +9,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("libreria")]
+[assembly: AssemblyTitle("EvilSalsa")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("libreria")]
-[assembly: AssemblyCopyright("Copyright 2018")]
+[assembly: AssemblyProduct("EvilSalsa")]
+[assembly: AssemblyCopyright("CyberVaca @ HackPlayers Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
